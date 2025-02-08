e24d7683bfc1a18fe5c56d897afd7d04b234bfc2
dgarcia024 <danielgv2404@gmail.com>
2025-02-08 11:48:09 -0600

    Cambio de acceso para variables

--- src/Ejercicio1.kt
+++ src/Ejercicio1.kt
@@ -4,29 +4,29 @@
  * con ellas, como sumas, concatenación y comparaciones.
  * */
 fun main() {
-    var numeroEntero: Int = 3
-    var numeroFlotante: Float = 5f
-    var numeroDoble: Double = 4.0
-    var cadena: String = "Hola"
-    var valor: Boolean = false
+    val numeroEntero: Int = 3
+    val numeroFlotante: Float = 5f
+    val numeroDoble: Double = 4.0
+    val cadena: String = "Hola"
+    val valor: Boolean = false
 
 
     // suma de 2 numeros
-    var suma1 = numeroEntero + numeroFlotante
-    var suma2 = numeroEntero + numeroDoble
-    var suma3 = numeroFlotante + numeroDoble
+    val suma1 = numeroEntero + numeroFlotante
+    val suma2 = numeroEntero + numeroDoble
+    val suma3 = numeroFlotante + numeroDoble
     println("$numeroEntero mas $numeroFlotante es igual a: $suma1")
     println("$numeroEntero mas $numeroDoble es igual a: $suma2")
     println("$numeroFlotante mas $numeroDoble es igual a: $suma3\n")
 
     //concatenacion
-    var concatenado = "$cadena $valor"
+    val concatenado = "$cadena $valor"
     println("concatenado = $concatenado\n")
 
     //comparaciones
-    var comparacion1 = numeroEntero > numeroFlotante
-    var comparacion2 = numeroFlotante < numeroDoble
-    var comparacion3 = numeroDoble > numeroEntero
+    val comparacion1 = numeroEntero > numeroFlotante
+    val comparacion2 = numeroFlotante < numeroDoble
+    val comparacion3 = numeroDoble > numeroEntero
     println("$numeroEntero es mayor que $numeroFlotante? = $comparacion1")
     println("$numeroFlotante es menor que $numeroDoble? = $comparacion2")
     println("$numeroDoble es mayor que $numeroEntero? = $comparacion3")
